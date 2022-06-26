6b2e1271aaa915c40643ca53cd7c350626532cf5
neo <neo@matrix.com>
2022-06-26 14:11:35 +0530

    Boiler plate code with Spring Web, JPA, and postgres Driver

--- src/main/java/com/learn/springBootBasics/dao/TicketDao.java
+++
@@ -1,7 +0,0 @@
-package com.learn.springBootBasics.dao;
-
-import com.learn.springBootBasics.model.Ticket;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TicketDao extends CrudRepository<Ticket, Integer> {
-}
